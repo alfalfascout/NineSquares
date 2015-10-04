f164d9244bbef59904a10fe97c0a3a9e742b60cd
Dhwani Shah <dshah202@gmail.com>
2015-10-04 17:18:21 -0500

    Not completed. Added the new color pallets, but the names are not final so the class name in css and the method names in colors.js and index.html are not verified

--- Web App/colors.js
+++ Web App/colors.js
@@ -17,3 +17,27 @@ function makeMonochrome() {
 bod.setAttribute("class", "monochrome");
 par.textContent = "black";
 }
+function makeRedPurple() {
+bod.setAttribute("class", "redpurple");
+par.textContent = "purple";
+}
+function makePurplePink() {
+bod.setAttribute("class", "purplepink");
+par.textContent = "pink";
+}
+function makePurpleYellow() {
+bod.setAttribute("class", "purpleyellow");
+par.textContent = "yellow";
+}
+function makeYellowBlue() {
+bod.setAttribute("class", "yellowblue");
+par.textContent = "blue";
+}
+function makeLightgreenGreen() {
+bod.setAttribute("class", "lightgreengreen");
+par.textContent = "darkgreen";
+}
+function makeWhiteLightyellow() {
+bod.setAttribute("class", "whitelightyellow");
+par.textContent = "yellow";
+}
