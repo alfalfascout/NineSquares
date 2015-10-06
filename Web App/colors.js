8e5ced7c4a7ca15935fb0eeac26f9908ded22e2f
alfalfascout <alfalfascouting@gmail.com>
2015-10-06 00:05:15 -0400

    Abbreviate color code, remember user's color pref, solves issue #7
    Abbreviates the color-changing code in colors.js and adds the ability
    to set local storage. Moves color css out of main.css to new
    colors.css. Changes the color-setting onclicks in index.html. + Minor
    changes

--- Web App/colors.js
+++ Web App/colors.js
@@ -1,43 +1,25 @@
 var bod = document.getElementsByTagName("body")[0];
 var par = document.getElementById("wincolor");
 
-function makeMidnight() {
-bod.setAttribute("class", "midnight");
-par.textContent = "teal";
+function updateColor() {
+    var colorScheme = localStorage.getItem("colorscheme");
+    var winColor = localStorage.getItem("wincolor");
+    bod.setAttribute("class", colorScheme);
+    par.textContent = winColor;
 }
-function makeDaylight() {
-bod.setAttribute("class", "daylight");
-par.textContent = "orange";
+
+function makeColor(colorScheme, winColor) {
+    localStorage.setItem("colorscheme", colorScheme);
+    localStorage.setItem("wincolor", winColor);
+    updateColor();
 }
-function makePastel() {
-bod.setAttribute("class", "pastel");
-par.textContent = "pink";
+
+function startColors() {
+    makeColor("monochrome", "black");
 }
-function makeMonochrome() {
-bod.setAttribute("class", "monochrome");
-par.textContent = "black";
-}
-function makeCheckers() {
-bod.setAttribute("class", "checkers");
-par.textContent = "red";
-}
-function makeGentlePinkle() {
-bod.setAttribute("class", "gentlepinkle");
-par.textContent = "pink";
-}
-function makePurpleDragon() {
-bod.setAttribute("class", "purpledragon");
-par.textContent = "purple";
-}
-function makeSilverGold() {
-bod.setAttribute("class", "silvergold");
-par.textContent = "blue";
-}
-function makeHappySapling() {
-bod.setAttribute("class", "happysapling");
-par.textContent = "dark green";
-}
-function makeWhiteGold() {
-bod.setAttribute("class", "whitegold");
-par.textContent = "white";
+
+if(!localStorage.getItem("colorscheme")) {
+    startColors();
+} else {
+    updateColor();
 }
